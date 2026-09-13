330cdc930ec44e99c6300775efff98a03ffca6cc
Tushar Pavaiya <pavaiyatushar329@gmail.com>
2026-09-13 12:42:22 +0530

    Time: 101 ms (13.19%) | Memory: 18.6 MB (69.12%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for(int i=0;i<matrix.size();i++){
+            int l=0;
+            int h=matrix[0].size()-1;
+            while(l<=h){
+                int mid=l+(h-l)/2;
+                if(matrix[i][mid]==target)return true;
+                else if(matrix[i][mid]<target){
+                    l=mid+1;
+                }else{
+                    h=mid-1;
+                }
+            }
+        }
+        return false;
+    }
+};
